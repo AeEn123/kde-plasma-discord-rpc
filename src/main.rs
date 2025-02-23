8da9dbbd1499baa9f9b0c029a5ac9e53a1ed8c9e
AeEn123 <63899598+AeEn123@users.noreply.github.com>
2025-02-23 21:14:17 +0000

    Change to other RPC thing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{thread::sleep, time};
-use discord_rpc_client::{Client, Event};
+use discord_presence::Client;
 
 fn main() {
     let kernel_version = std::fs::read_to_string("/proc/sys/kernel/osrelease").unwrap();
@@ -22,25 +22,31 @@ fn main() {
     let distro = distro.iter().find(|&&x| x.contains("PRETTY_NAME")).unwrap();
     let distro = distro.replace("PRETTY_NAME=", "").replace("\"", "");
 
-
-
     let display_server = std::env::var("XDG_SESSION_TYPE").unwrap();
+
     // Create the client
     let mut drpc = Client::new(1322629749612351538);
 
     // Register event handlers with the corresponding methods
     drpc.on_ready(|_ctx| {
         println!("ready");
-    });
+    }).persist();
 
     // Start up the client connection, so that we can actually send and receive stuff
     drpc.start();
 
-    // Set the activity
-    drpc.set_activity(|act| act.state(format!("{} | {}",distro, kernel_version)).details(format!("{} | KWin {} ({})", plasma_version, kwin_version, display_server)))
-        .expect("Failed to set activity");
+
 
     loop {
-        sleep(time::Duration::from_secs(1));
+        // Set the activity
+        if let Err(e) = drpc.set_activity(|act|
+            act.state(format!("{} | {}",distro, kernel_version))
+            .details(format!("{} | KWin {} ({})", plasma_version, kwin_version, display_server))
+           // .append_buttons(|button| button.label("Click Me!").url("https://google.com/"))
+        ) {
+            println!("Failed to set activity: {}", e)
+        }
+        
+        sleep(time::Duration::from_secs(10));
     }
 }
